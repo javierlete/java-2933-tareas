186e816c801f76a5f4a09cfff3b464b88b731666
Javier Lete <javier@eskura21.com>
2026-06-15 13:05:16 +0200

    Utilización de fetch en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Tarea from './componentes/Tarea';
 import Tareas from './componentes/Tareas';
-import { TAREAS } from './mocks/tareas';
+import { URL_TAREAS } from './constantes/rutas';
 
 export default function App() {
-  const [tareas, setTareas] = useState(TAREAS);
+  const [tareas, setTareas] = useState([]);
   const [descripcion, setDescripcion] = useState('');
 
-  function alAnadir() {
-    const id = tareas.map(tarea => tarea.id).reduce((anterior, actual) => anterior > actual ? anterior: actual) + 1;
-    setTareas([{ id, descripcion, completada: false }, ...tareas]);// tareas.push({id: 0, descripcion, completada: false});
+  useEffect(() => {
+    fetch(URL_TAREAS).then(respuesta => {
+      respuesta.json().then(tareas => {
+        setTareas(tareas);
+      })
+    })
+  }, []);
+
+  async function alAnadir() {
+    // const id = tareas.map(tarea => tarea.id).reduce((anterior, actual) => anterior > actual ? anterior : actual) + 1;
+
+    const tarea = { descripcion, completada: false };
+
+    const respuesta = await fetch(URL_TAREAS, {
+      method: 'POST',
+      body: JSON.stringify(tarea),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+
+    const tareaNueva = await respuesta.json();
+
+    setTareas([tareaNueva, ...tareas]);// tareas.push({id: 0, descripcion, completada: false});
     setDescripcion('');
   }
 
---
+++ src/constantes/rutas.js
@@ -0,0 +1,1 @@
+export const URL_TAREAS = 'https://6a2fd7afa7f8866418d52aae.mockapi.io/api/v1/tareas/';
